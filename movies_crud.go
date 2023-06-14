34d12475eebe941c0544277b8d75867e0be2e9ad
Ammar Arsiwala <94366084+ammar-arsiwala@users.noreply.github.com>
2023-06-14 17:50:59 +0530

    Update movies_crud.go
    fixed bugs

--- movies_crud.go
+++ movies_crud.go
@@ -28,15 +28,17 @@ var movies []Movie
 
 func main(){
 	r:=mux.NewRouter()
-	movie=append(movies, Movie{ID: "1",Isbn"12323", Title: "Armour of God 1",Director: &Director{FirstName:"Jackie",LastName:"Chan",Country:"China"}})	
-	movie=append(movies, Movie{ID: "2",Isbn"12324", Title: "Armour of God 2",Director: &Director{FirstName:"Jackie",LastName:"Chan",Country:"China"}})	
+	movie=append(movies, Movie{ID: "1",Isbn:"12323", Title: "Armour of God 1",Director: &Director{FirstName:"Jackie",LastName:"Chan",Country:"China"}})	
+	movie=append(movies, Movie{ID: "2",Isbn:"12324", Title: "Armour of God 2",Director: &Director{FirstName:"Jackie",LastName:"Chan",Country:"China"}})	
 	r.HandleFunc("/movies",GetMovies).Methods(GET)
 	r.HandfleFunc("/movies/{id}",GetMovie).Methods(GET)
 	r.HandleFunc("/movies",CreateMovies).Methods(CREATE)
 	r.HandleFunc("/movies/{id}",UpdateMovie).Methods(UPDATE)
 	r.HandleFunc("/movies/{id}",DeleteMovies).Methods(DELETE)
-}
 
+fmt.Printf("starting server at 8000\n")
+fatal.log(http.ListenAndServeTLS(":8000,r"))
+}
 
 func GetMovies(w http.ResponseWriter,r *http.Request){
 	w.Header().Set("Content-Type","applcation/json")
@@ -47,19 +49,17 @@ func GetMovies(w http.ResponseWriter,r *http.Request){
 func DeleteMovies(w http.ResponseWriter,r *http.Request){
 	w.Header().Set("Content-Type","application/json")
 	params:= Mux.vars(r)
-	for index, item:= range movies
-	{
+	for index, item:= range movies{
 		if items.ID == params["id"]{
-			movies=append[movies[:index],movies[:index + 1]...]
+			movies=append(movies[:index], movies[index+1:]...)
 			break
 		} 
-		else 
-			movies[:index++]
+	
 	}
 }
 
 
-func GetMovies( w http.ResponseWriter), (r * http.Request){
+func GetMovies( w http.ResponseWriter,r * http.Request){
 	w.Header().Set("Content-Type","application/json")
 	params:= Mux.vars(r)
 	for _,item:= range movies
@@ -90,7 +90,7 @@ func UpdateMovies(w http.ResponseWriter, r * http.Request){
 	for index,item:= range movies
 	{
 		if items.ID == params["id"]{
-			movies = append[movies[:index],movies[:index + 1]...]
+			movies = append(movies[:index],movies[index + 1:]...)
 			var Movie movies
 			_=json.NewDecoder(r.body)Decode(&movie)
 			  movie.ID = params["id"]
